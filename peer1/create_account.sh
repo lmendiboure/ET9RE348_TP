e444c031a3814e9869630c03fd2830f95ee226d0
root <root@exp3-12.retel.enseirb>
2018-11-13 09:02:34 +0100

    Updating account creation

--- peer1/create_account.sh
+++ peer1/create_account.sh
@@ -1,1 +1,8 @@
-geth --datadir "~/.ethereum_example" --password "./peer1/ethereum_pwd.txt" account new
+#!/bin/bash
+uname=$(echo $(uname -a))
+if [[ $uname =~ "Ubuntu" ]]; then
+	geth --datadir "~/.ethereum_example" --password "./peer1/ethereum_pwd.txt" account new
+elif [[ $uname =~ "Debian" ]]; then
+	./geth --datadir "~/.ethereum_example" --password "./peer1/ethereum_pwd.txt" account new
+fi
+
