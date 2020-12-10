b2618a31f6f937073c9e098267d3ffe5903ed929
lmendiboure <leo.mendiboure11@gmail.com>
2020-12-10 15:39:12 +0100

    Update run.sh

--- peer2/run.sh
+++ peer2/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 uname=$(echo $(uname -a))
 if [[ $uname =~ "Ubuntu" ]]; then
-    geth --identity "BlockchainTP" --rpc --rpcport 8546 --datadir "~/.ethereum_example_2" --port 30307 --nodiscover --rpcapi "db,personal,eth,net,web3,debug" --rpccorsdomain="*" --ipcdisable --rpcaddr="localhost" --networkid 1999 console --allow-insecure-unlock
+    geth --identity "BlockchainTP" --rpc --rpcport 8546 --datadir "~/.ethereum_example_2" --port 30307 --nodiscover --rpcapi "db,personal,eth,net,web3,debug" --rpccorsdomain="*" --ipcdisable --rpcaddr="localhost" --networkid 1999 console
 elif [[ $uname =~ "Debian" ]]; then
-    ./geth --identity "BlockchainTP" --rpc --rpcport 8546 --datadir "~/.ethereum_example_2" --port 30307 --nodiscover --rpcapi "db,personal,eth,net,web3,debug" --rpccorsdomain="*" --ipcdisable --rpcaddr="localhost" --networkid 1999 console --allow-insecure-unlock
+    ./geth --identity "BlockchainTP" --rpc --rpcport 8546 --datadir "~/.ethereum_example_2" --port 30307 --nodiscover --rpcapi "db,personal,eth,net,web3,debug" --rpccorsdomain="*" --ipcdisable --rpcaddr="localhost" --networkid 1999 console 
 fi
