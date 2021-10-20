b0277d2a868d3078c717a0ad1050bf7d8813e97a
lmendiboure <leo.mendiboure11@gmail.com>
2021-10-20 16:29:04 +0200

    Update create_account.sh

--- peer1/create_account.sh
+++ peer1/create_account.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-uname=$(echo $(uname -a))
-if [[ $uname =~ "Ubuntu" ]]; then
-	geth --datadir "~/.ethereum_example" --password "./peer1/ethereum_pwd.txt" account new
-elif [[ $uname =~ "Debian" ]]; then
-	./geth --datadir "~/.ethereum_example" --password "./peer1/ethereum_pwd.txt" account new
-fi
+geth --datadir "~/.ethereum_example" --password "./peer1/ethereum_pwd.txt" account new
 
