9c02fc0d585306136fc611b505f46987179f59e7
lmendiboure <leo.mendiboure11@gmail.com>
2021-10-20 16:30:06 +0200

    Simplify run.sh

--- peer2/run.sh
+++ peer2/run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-uname=$(echo $(uname -a))
-if [[ $uname =~ "Ubuntu" ]]; then
-    geth --identity "BlockchainTP" --rpc --rpcport 8546 --datadir "~/.ethereum_example_2" --port 30307 --nodiscover --rpcapi "db,personal,eth,net,web3,debug" --rpccorsdomain="*" --ipcdisable --rpcaddr="localhost" --networkid 1999 console
-elif [[ $uname =~ "Debian" ]]; then
-    ./geth --identity "BlockchainTP" --rpc --rpcport 8546 --datadir "~/.ethereum_example_2" --port 30307 --nodiscover --rpcapi "db,personal,eth,net,web3,debug" --rpccorsdomain="*" --ipcdisable --rpcaddr="localhost" --networkid 1999 console 
-fi
+geth --identity "BlockchainTP" --rpc --rpcport 8546 --datadir "~/.ethereum_example_2" --port 30307 --nodiscover --rpcapi "db,personal,eth,net,web3,debug" --rpccorsdomain="*" --ipcdisable --rpcaddr="localhost" --networkid 1999 console 
+
